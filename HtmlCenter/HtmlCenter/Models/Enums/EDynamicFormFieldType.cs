b493a63ebf36dd71349f961019ab2f2224fff7ce
皓中 吳 <s7514381@gmail.com>
2023-01-25 15:37:25 +0800

    n

---
+++ HtmlCenter/HtmlCenter/Models/Enums/EDynamicFormFieldType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HtmlCenter.Models.Enums
+{
+    public enum EDynamicFormFieldType
+    {
+        [Display(Name = "文字輸入")]
+        Input = 1,
+
+        [Display(Name = "多行輸入")]
+        Textarea = 2,
+
+        [Display(Name = "單一選擇")]
+        Select = 3,
+
+        [Display(Name = "多重選擇")]
+        MultiSelect = 4,
+    }
+}

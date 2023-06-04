bd0f5464b6faa62a778b8ffa4327d97b66d705c5
皓中 吳 <s7514381@gmail.com>
2023-06-05 00:44:06 +0800

    聊天大廳

--- HtmlCenter/HtmlCenter/wwwroot/js/firebase/database.js
+++ HtmlCenter/HtmlCenter/wwwroot/js/firebase/database.js
@@ -1,58 +1,51 @@
 ﻿import { app } from "/js/firebase/app.js";
-import { getDatabase, ref, onValue, set, serverTimestamp, onDisconnect, remove, push } from "/lib/firebase/gstatic.com_firebasejs_9.22.1_firebase-database.js";
+import {
+    getDatabase, ref, onValue, set, serverTimestamp, onDisconnect, remove
+    , push, onChildAdded, onChildChanged, onChildRemoved, child, get
+} from "/lib/firebase/gstatic.com_firebasejs_9.22.1_firebase-database.js";
 
 // 獲取 Realtime Database 的根節點引用
 const db = getDatabase(app);
 
-function dbConnection(dbName, func) {
+function dbConnection(dbName, valueFunc = null, addFunc = null, removeFunc = null, updateFunc = null) {
     const connectionsRef = ref(db, dbName);
 
-    // 當節點數據變化時觸發的回調函數
-    onValue(connectionsRef, async (snapshot) => {
-        const snapshotVal = snapshot.val();
-        for (const key in snapshotVal) {
-            await func(key, snapshotVal[key]);
-        }
-    });
+    if (valueFunc) {
+        onValue(connectionsRef, async (snapshot) => {
+            await valueFunc(snapshot.val());
+        }, {
+            onlyOnce: true
+        });
+    }
+ 
+    if (addFunc) {
+        onChildAdded(connectionsRef, async (snapshot) => {
+            await addFunc(snapshot.val());
+        });
+    }
+
+    if (removeFunc) {
+        onChildRemoved(connectionsRef, async (snapshot) => {
+            await removeFunc(snapshot.val())
+        });
+    }
+    
+    if (updateFunc) {
+        onChildChanged(connectionsRef, async (snapshot) => {
+            await updateFunc(snapshot.val());
+        });
+    }
+ 
 }
 
 // 添加用戶連接
 function addUserConnection(dbName, userId, data) {
     const connectionsRef = ref(db, `${dbName}/${userId}`);
-    const lastOnlineRef = ref(db, 'users/joe/lastOnline');
-
-    set(connectionsRef, data);
-
-    //onDisconnect(connectionsRef).update({ connections: false }).then(function (v) {
-    //    set(lastOnlineRef, true);
-    //    //remove(connectionsRef);
-    //});
     onDisconnect(connectionsRef).remove()
-    //onDisconnect(connectionsRef).remove().then(() => {
-    //    //set(connectionsRef, null).the n(() => {
-    //    //    console.log('使用者移除成功');
-    //    //}).catch((error) => {
-    //    //    console.log('使用者移除時發生錯誤：', error);
-    //    //});
-
-    //    set(lastOnlineRef, {qq: 123});
-
-    //    //if (error) {
-    //    //    console.log('移除斷線時出現錯誤：', error);
-    //    //} else {
-    //    //    console.log('成功設定斷線事件');
-    //    //}
-    //});
+    set(connectionsRef, data);
 }
 
-
-
-
-
-function removeConnection(dbName, userId) {
-    remove(ref(db, `${dbName}/${userId}`));
-}
 
 export const exportModel = {
-    addUserConnection, dbConnection, serverTimestamp, onDisconnect, removeConnection
+    addUserConnection, dbConnection, serverTimestamp, onDisconnect
 }
--- js/firebase/database.js
+++ js/firebase/database.js
@@ -1,45 +1,51 @@
 ﻿import { app } from "/js/firebase/app.js";
-import { getDatabase, ref, onValue, set, serverTimestamp, onDisconnect, remove } from "/lib/firebase/gstatic.com_firebasejs_9.22.1_firebase-database.js";
+import {
+    getDatabase, ref, onValue, set, serverTimestamp, onDisconnect, remove
+    , push, onChildAdded, onChildChanged, onChildRemoved, child, get
+} from "/lib/firebase/gstatic.com_firebasejs_9.22.1_firebase-database.js";
 
 // 獲取 Realtime Database 的根節點引用
-const database = getDatabase(app);
+const db = getDatabase(app);
 
-// 監聽用戶連接狀態
-function trackUserConnections() {
-    const connectionsRef = ref(database, "connections");
+function dbConnection(dbName, valueFunc = null, addFunc = null, removeFunc = null, updateFunc = null) {
+    const connectionsRef = ref(db, dbName);
 
-    // 當節點數據變化時觸發的回調函數
-    onValue(connectionsRef, (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-            const userId = childSnapshot.key;
-            console.log(`User connected: ${userId}`);
+    if (valueFunc) {
+        onValue(connectionsRef, async (snapshot) => {
+            await valueFunc(snapshot.val());
+        }, {
+            onlyOnce: true
         });
-    });
+    }
+ 
+    if (addFunc) {
+        onChildAdded(connectionsRef, async (snapshot) => {
+            await addFunc(snapshot.val());
+        });
+    }
+
+    if (removeFunc) {
+        onChildRemoved(connectionsRef, async (snapshot) => {
+            await removeFunc(snapshot.val())
+        });
+    }
+    
+    if (updateFunc) {
+        onChildChanged(connectionsRef, async (snapshot) => {
+            await updateFunc(snapshot.val());
+        });
+    }
+ 
 }
 
 // 添加用戶連接
-function addUserConnection(userId) {
-    const connectionsRef = ref(database, `connections/${userId}`);
-
-    set(connectionsRef, {
-        connected: true,
-        timestamp: serverTimestamp(),
-    }); 
-
-    // 在用戶斷開連接時刪除用戶連接信息
-    onDisconnect(connectionsRef).remove();
+function addUserConnection(dbName, userId, data) {
+    const connectionsRef = ref(db, `${dbName}/${userId}`);
+    onDisconnect(connectionsRef).remove()
+    set(connectionsRef, data);
 }
 
-// 測試用戶連接狀態追踪
-trackUserConnections();
 
-//// 添加用戶連接
-//addUserConnection("user1");
-//addUserConnection("user2");
-
-// 假裝斷開 user1 的連接（模擬用戶離線）
-//setTimeout(() => {
-//    remove(ref(database, "connections/user1"));
-//}, 5000);
-
-//export const rdAssembly
+export const exportModel = {
+    addUserConnection, dbConnection, serverTimestamp, onDisconnect
+}

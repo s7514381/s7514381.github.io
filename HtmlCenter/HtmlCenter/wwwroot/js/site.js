3549aaab0418f5e8ddd0c06edef9cf274404c1b8
皓中 吳 <s7514381@gmail.com>
2023-06-08 17:57:44 +0800

    n

--- HtmlCenter/HtmlCenter/wwwroot/js/site.js
+++ HtmlCenter/HtmlCenter/wwwroot/js/site.js
@@ -1,13 +1,3 @@
 ﻿function DeepClone(model) {
     return JSON.parse(JSON.stringify(model));
 }
-
-async function getIpClient() {
-    let result;
-    try {
-        result = await axios.get('https://api.ipify.org?format=json');
-    } catch (error) {
-        console.error(error);
-    }
-    return result;
-}
